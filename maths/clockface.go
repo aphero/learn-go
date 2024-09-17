12dd6c4e5d1d55c3f389f46673f21b1f02fdfad9
aphero <jmckelvey0@gmail.com>
2024-09-16 21:25:23 -0400

    finish hour hand

--- maths/clockface.go
+++ maths/clockface.go
@@ -18,35 +18,42 @@ func secondsInRadians(t time.Time) float64 {
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
-	x := math.Sin(angle)
-	y := math.Cos(angle)
-	return Point{x, y}
+	return angleToPoint(secondsInRadians(t))
 }
 
 // Minutes
 
 func minuteHandPoint(t time.Time) Point {
-	return AngleToPoint(minutesInRadians(t))
+	return angleToPoint(minutesInRadians(t))
 }
 
 func minutesInRadians(t time.Time) float64 {
-	return (math.Pi / (30 / float64(t.Minute())))
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) +
+		(math.Pi / (6 / float64(t.Hour()%12)))
 }
 
 // WHERE DO THESE GO?
 
-func AngleInRadians(t time.Time) float64 {
-	// Here we're converting the seconds to radians and dividing by 60 to get the one minute adjustment
-	// for the minute hand.  Not dividing by 60 gives us the angle of the second hand.
-	return (secondsInRadians(t) / 60) +
-		// Then we divide 30 by the minute we're rendering so we know how many radians we're dealing with
-		// and then divide PI by the result to get the actual radians that our angle is at, just like we
-		// do for the second hand calculation.
-		(math.Pi / (30 / float64(t.Minute())))
-}
+// func AngleInRadians(t time.Time) float64 {
+// 	// Here we're converting the seconds to radians and dividing by 60 to get the one minute adjustment
+// 	// for the minute hand.  Not dividing by 60 gives us the angle of the second hand.
+// 	return (secondsInRadians(t) / 60) +
+// 		// Then we divide 30 by the minute we're rendering so we know how many radians we're dealing with
+// 		// and then divide PI by the result to get the actual radians that our angle is at, just like we
+// 		// do for the second hand calculation.
+// 		(math.Pi / (30 / float64(t.Minute())))
+// }
 
-func AngleToPoint(angle float64) Point {
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+// Seconds tests
+
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
 		time  time.Time
@@ -46,6 +48,8 @@ func TestSecondHandPoint(t *testing.T) {
 	}
 }
 
+// Minutes tests
+
 func TestMinutesInRadians(t *testing.T) {
 	cases := []struct {
 		time  time.Time
@@ -77,6 +81,47 @@ func TestMinuteHandPoint(t *testing.T) {
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
 			got := minuteHandPoint(c.time)
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+}
+
+// Hours tests
+
+func TestHoursInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(6, 0, 0), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(21, 0, 0), math.Pi * 1.5},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hoursInRadians(c.time)
+			if !roughlyEqualFloat64(got, c.angle) {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+
+func TestHourHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(6, 0, 0), Point{0, -1}},
+		{simpleTime(21, 0, 0), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hourHandPoint(c.time)
 			if !roughlyEqualPoint(got, c.point) {
 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
 			}
